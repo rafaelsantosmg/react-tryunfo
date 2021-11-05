56fc50dc75825895018f2dde91da51ce454a1d29
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-11-05 16:22:12 -0300

    build: Feita a implementação das div necessarias para manipular o App.css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import Form from './components/Form';
+import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="page">
+        <header className="header">
           <h1>Tryunfo</h1>
-        </div>
-        <div>
-          <Form />
-        </div>
+        </header>
+        <main className="main">
+          <div className="form">
+            <Form />
+          </div>
+          <div className="preview">
+            <h2>Preview</h2>
+          </div>
+        </main>
       </div>
     );
   }
